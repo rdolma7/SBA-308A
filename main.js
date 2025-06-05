213600f67c89ec0f3491d9daa34075ffbe3d3cbf
Rinzin Dolma <rsamrab7@gmail.com>
2025-06-05 16:29:26 -0400

    Added the dropdown and the amount field

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 let body = document.querySelector("body");
-const h1=document.createElement("h1");
-h1.textContent="Currency Exchange Application"
-body.appendChild(h1)
 
+const btn = document.createElement("button");
+btn.textContent = "Exchange Now";
+body.appendChild(btn);
 
-const input = document.createElement("input");
-input.setAttribute("id", "initialValue");
-input.type = "text";
-input.placeholder = "From";
-body.appendChild(input);
+let url =
+  "https://api.currencyfreaks.com/v2.0/supported-currencies?apikey=d39a9be0c13741d28839052a910cb9b7";
 
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-const btn=document.createElement("button");
-btn.textContent="Exchange Now";
-body.appendChild(btn)
-
-let url = `https://api.currencyfreaks.com/v2.0/supported-currencies?apikey=d39a9be0c13741d28839052a910cb9b7`;
-const API_KEY = "d39a9be0c13741d28839052a910cb9b7";
 
 async function currencyExchange() {
   let result = await fetch(url);
   let currencies = await result.json();
-  for (currency of currencies) {
-    const option = document.createElement("option");
-    option.value = currency.countryCode;
-    option.textContent = option.value;
-    body.appendChild(option);
+  //   for (let currency of Object.entries(currencies.supportedCurrenciesMap)) {
+  //     console.log(currency);
+  //     await delay(1000);
+  //   };
+  // console.log(currencies)
+  let currenciesArray = Object.entries(currencies.supportedCurrenciesMap);
+  let countryCurrencies = currenciesArray.filter(
+    (currency) =>
+      currency[1].countryCode !== "Crypto" &&
+      currency[1].countryCode !== "metal" &&
+      currency[1].countryCode !== null
+  );
+  let index = 0
+  for (let currency of countryCurrencies) {
+    console.log(currency)
+    let option = document.createElement("option");
+    option.value=currency[1].countryName;
+    option.textContent = currency[1].countryName;
+    
+    index++
+    document.getElementById("convertFrom").appendChild(option)
+    document.getElementById("convertTo").appendChild(option)
+    if(index==55)break
+    // console.log(currency);
+    // await delay(1000);
   }
-
-  console.log(currency); //filter out all the ones with country code crypto
-  //
 }
 currencyExchange();
 
-// for (result of data.results) {
-//     // console.log(result.percentage);
-//     if (result.percentage > maxPercent) {
-//     //   console.log(maxPercent);
-//       maxPercent = result.percentage;
-//       language = result;
-//     }
-//   }
-//   console.log(language);
-
-// let url = `https://api.languagelayer.com/languages?access_key=7a9f65600136e76725567cf0cb79fef6`;
-
-// let text = "Hi";
-// let url = `https://api.languagelayer.com/detect?access_key=7a9f65600136e76725567cf0cb79fef6&query=${text}`;
-// const API_KEY = "7a9f65600136e76725567cf0cb79fef6";
+// async function exchangeNow(currency) {
+//   let amount = 
+//   let exchangeRate = await fetch ('https://api.currencyfreaks.com/v2.0/convert/latest?apikey=YOUR_APIKEY&from=USD&to=PKR&amount=')
+// }
