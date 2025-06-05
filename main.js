392d9f5f967f564a028cac6c661573560d22b264
Rinzin Dolma <rsamrab7@gmail.com>
2025-06-05 16:58:16 -0400

    both dropdowns working. tried to implement the function exchange now but it is not working yet.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let body = document.querySelector("body");
-
+let convertFrom = document.querySelector("#convertFrom")
+let convertTo= document.querySelector("#convertTo")
 const btn = document.createElement("button");
 btn.textContent = "Exchange Now";
 body.appendChild(btn);
-
 let url =
   "https://api.currencyfreaks.com/v2.0/supported-currencies?apikey=d39a9be0c13741d28839052a910cb9b7";
+  async function exchangeNow() {
+  let exchange = await fetch (`${url}&from=${convertFrom.value}&to=${convertTo.value}&amount=${amount}`)
+  }
+btn.addEventListener("click", (event)=>{
+  event.preventDefault()
+  exchangeNow(amount);
+})
+
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -34,17 +42,21 @@ async function currencyExchange() {
     option.value=currency[1].countryName;
     option.textContent = currency[1].countryName;
     
+
     index++
     document.getElementById("convertFrom").appendChild(option)
-    document.getElementById("convertTo").appendChild(option)
+
+    let option2 = option.cloneNode(true)
+  
+    
+    document.getElementById("convertTo").appendChild(option2)
     if(index==55)break
     // console.log(currency);
     // await delay(1000);
   }
+  document.getElementById("convertTo").value ="Latvia" 
 }
 currencyExchange();
 
-// async function exchangeNow(currency) {
-//   let amount = 
-//   let exchangeRate = await fetch ('https://api.currencyfreaks.com/v2.0/convert/latest?apikey=YOUR_APIKEY&from=USD&to=PKR&amount=')
-// }
+
+
