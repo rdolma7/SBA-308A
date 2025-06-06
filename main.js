4c6aa23df9124c85d42752008eb16689278f2f38
Rinzin Dolma <rsamrab7@gmail.com>
2025-06-06 16:02:09 -0400

    Added the flag to show up of the country selected

--- main.js
+++ main.js
@@ -1,52 +1,42 @@
-let body = document.querySelector("body");
-let convertFrom = document.querySelector("#convertFrom")
-let convertTo= document.querySelector("#convertTo")
-const btn = document.createElement("button");
- let input = document.querySelector("input")
-btn.textContent = "Exchange Now";
-body.appendChild(btn);
-// const url = 'https://exchange-rates7.p.rapidapi.com/convert?base=USD&target=JPY';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': 'b867df96b8mshb77efccd8066deep1385fajsne155a4a0b1a1',
-// 		'x-rapidapi-host': 'exchange-rates7.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
 let url =
   "https://api.currencyfreaks.com/v2.0/supported-currencies?apikey=d39a9be0c13741d28839052a910cb9b7";
-  async function exchangeNow() {
-  let exchange = await fetch (`https://api.currencyfreaks.com/v2.0/convert/latest?apikey=d39a9be0c13741d28839052a910cb9b7&from=${convertFrom.value}&to=${convertTo.value}&amount=${input.value}`)
-  let exchangeData= await exchange.json();
-  console.log(exchangeData)
-  }
-btn.addEventListener("click", (event)=>{
-  event.preventDefault();
-  exchangeNow();
-})
 
+let body = document.querySelector("body");
+let convertFrom = document.querySelector("#convertFrom");
+let convertTo = document.querySelector("#convertTo");
+let input = document.querySelector("input");
+let select = document.querySelector("select");
+
+const btn = document.createElement("button");
+btn.textContent = "Enter";
+body.appendChild(btn);
+// const btn=document.getElementsByTagName("button")
+let div = document.createElement("div");
+div.id = "flagContainer";
+body.appendChild(div);
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  // showCountryIcon();
+  let currentCurrencySelected = document.getElementById("convertFrom");
+  // console.log(allCurrencyData)
+  let image = document.createElement("img");
+  image.src = allCurrencyData[currentCurrencySelected.value].icon;
+
+  div.appendChild(image);
+  console.log(div.children.length);
+  while (div.children.length > 1) {
+    div.removeChild(div.firstElementChild);
+  }
+});
+
+body.appendChild(btn);
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-
-async function currencyExchange() {
+async function getCurrencies() {
   let result = await fetch(url);
   let currencies = await result.json();
-  //   for (let currency of Object.entries(currencies.supportedCurrenciesMap)) {
-  //     console.log(currency);
-  //     await delay(1000);
-  //   };
-  // console.log(currencies)
   let currenciesArray = Object.entries(currencies.supportedCurrenciesMap);
   let countryCurrencies = currenciesArray.filter(
     (currency) =>
@@ -54,28 +44,44 @@ async function currencyExchange() {
       currency[1].countryCode !== "metal" &&
       currency[1].countryCode !== null
   );
-  let index = 0
-  for (let currency of countryCurrencies) {
-    console.log(currency)
+
+  countryCurrencies = countryCurrencies.slice(0, 15);
+
+  let currenciesData = {};
+  for (currency of countryCurrencies) {
+    currenciesData[currency[0]] = currency[1];
+  }
+  return currenciesData;
+}
+
+async function setCurrencyOptions(currencyData) {
+  for (let currency of Object.entries(currencyData)) {
     let option = document.createElement("option");
-    option.value=currency[1].currencyCode;
+    option.value = currency[0];
     option.textContent = currency[1].countryName;
-    
-
-    index++
-    document.getElementById("convertFrom").appendChild(option)
-
-    let option2 = option.cloneNode(true)
-  
-    
-    document.getElementById("convertTo").appendChild(option2)
-    if(index==100)break
+    option.id = "currencyOption";
+    document.getElementById("convertFrom").appendChild(option);
+    // let option2 = option.cloneNode(true)
+    // document.getElementById("convertTo").appendChild(option2)
     // console.log(currency);
     // await delay(1000);
   }
-  document.getElementById("convertTo").value ="MUR"
+  // document.getElementById("convertTo").value ="MUR"
 }
-currencyExchange();
+let allCurrencyData;
 
+getCurrencies().then((result) => {
+  allCurrencyData = result;
+  setCurrencyOptions(result);
+});
 
-
+// async function showCountryIcon() {
+//   // let countryCurrencies = await getCurrencies();
+//   // let countryIcons ={};
+//   // for(currency of countryCurrencies){
+//   //   countryIcons[currency[0]]=currency[1].icon;
+// }
+// let countryIcons = countryCurrencies.m((currency) => {
+//   return [currency[0], currency[1].icon];
+// });
+// console.log(countryIcons);
